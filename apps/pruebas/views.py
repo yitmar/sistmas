bf045350fb8711e741d10b14d3f8d994a52c8a7b
yitmar <yitmar.14151819@gmail.com>
2018-07-23 16:35:20 -0400

    modificaiones en prueba

--- apps/pruebas/views.py
+++ apps/pruebas/views.py
@@ -107,66 +107,82 @@ def vista_ver_pruebas_asignadas(request):
 
 def datos(request,pk):
     pruebs_presona=prueba_presona.objects.filter(id_prueba_presona=pk)
-    arreglo_pregunta=[]
-    preguntaa=[]
-    respuestas=[]
-    cantidad_pregunta=0
+    arreglo_respuesta=[]
     durancion_pruaba=0
-    arreglo_valor=[]
+    cantidad_pregunta=0
+    arreglo_pruebas=[]
+    arreglo_valor_respuesta=[]
+    arreglo_pregunta_respuesta=[]
+    id_categoria=0
+    id_participante=0
+    arreglo_pregunta=[]
     arreglo_preguntas=[]
+
+    prueba_presentar=[]
 
     for item in pruebs_presona:
         id_participante=item.id_participante
         id_prueba=item.id_prueba
-        print(id_prueba)
-        pruebas=Prueba.objects.filter(id_prueba=id_prueba)
-        print (pruebas)
+        p=str(id_prueba)
+        pruebas=Prueba.objects.filter(id_prueba=p)
         for prue in pruebas:
             durancion_pruaba=prue.durancion_pruaba
             cantidad_pregunta=prue.cantidad_pregunta
             arreglo_preguntas=prue.arreglo_preguntas
             arreglo_valor=prue.arreglo_valor
-            print(arreglo_valor)
-            print(arreglo_preguntas)
+            id_categoria=prue.id_categoria
         q=0
+        arreglo_pregun=len(arreglo_preguntas)-1
+
         while q < cantidad_pregunta:
-            arreglo_pregun=len(arreglo_preguntas)-1
-            numero_pregunta_rando= randint(0,arreglo_pregun)
-            arreglo_pregunta.append(arreglo_preguntas[numero_pregunta_rando])
-            id_pregunta=arreglo_pregunta[numero_pregunta_rando]
+            numero_pregunta_rando=randint(0,arreglo_pregun)
+            id_pregunta=arreglo_preguntas[numero_pregunta_rando]
+            
+            nuemro_pregunta=q+1
+            prueba_presentar.append(nuemro_pregunta)
+
+            arreglo_valor_respuesta.append(arreglo_valor[numero_pregunta_rando])
+            arreglo_pregunta_respuesta.append(id_pregunta)
 
             preguntaa=pregunta.objects.filter(id_pregunta=id_pregunta)
-            for pre in preguntaa:
-                nombre=pre.nombre_pregunta
-                tipo_pregunta=pre.tipo_pregunta
-                
+            arreglo_pregunta.append(preguntaa)
+            for pregun in preguntaa:
+                id_pregunta=pregun.id_pregunta
+                nombre_pregunta=pregun.nombre_pregunta
+                prueba_presentar.append(nombre_pregunta)
+
             respuestas=respuesta.objects.filter(id_pregunta=id_pregunta)
+            arreglo_respuesta.append(respuestas)
+            print(len(respuestas))
             for res in respuestas:
                 id_respuesta=res.id_respuesta
                 nombre_respuesta=res.nombre_respuesta
-                tipo_respuesta=res.tipo_respuesta
+                prueba_presentar.append(id_respuesta)
+                prueba_presentar.append(nombre_respuesta)                
+
             q=1+q
+
     if request.method == 'POST':
         a=0
         nota_evaluacion=0
         nota=0
         arreglo_respuesta=[]
+        print(request.POST)
         while a < cantidad_pregunta:
-            b=request.POST['respuesta']
-            arreglo_respuesta.append(b)
-            consulta_nota=respuesta.objects.filter(id_respuesta=b)
-            for consulta_de in consulta_nota:
-                if(consulta_de.tipo_respuesta):
-                    nota=1
+            pregunta_id=arreglo_pregunta_respuesta[a]
+            print(pregunta_id)
+            respuesta_pregunta=request.POST
+            arreglo_respuesta.append(respuesta_pregunta)
             nota_evaluacion=nota+nota_evaluacion  
             a=a+1
-        nueva_resultado=resultado(id_participante=id_participante,id_categoria=id_categoria,arreglo_preguntas=arreglo_pregunta,arreglo_respuesta=arreglo_respuesta,nota_evaluacion=nota_evaluacion)
-        nueva_resultado.save()
-        send_email(request)
+        nueva_resultado=resultado(id_participante=id_participante,id_categoria=id_categoria,arreglo_preguntas=arreglo_pregunta_respuesta,arreglo_respuesta=arreglo_respuesta,nota_evaluacion=nota_evaluacion)
+        #nueva_resultado.save()
+        #send_email(request)
         return redirect('/')
     else:
+        print(prueba_presentar)
         return render(request, 'pruebas/ver_prueba.html',
-        {'preguntaa':preguntaa,'respuestas':respuestas,'prueba':prueba,'datos_particpante':datos_particpante},)
+        {'arreglo_pregunta':arreglo_pregunta,'arreglo_respuesta':arreglo_respuesta,'pruebs_presona':pruebs_presona,'arreglo_pruebas':arreglo_pruebas,'prueba_presentar':prueba_presentar },)
 
 def send_email(request):
     msg=EmailMessage(subject="prueba",from_email="yitmar.14151819@gmail.com",to=['yitmar.14151819@hotmail.com'])
